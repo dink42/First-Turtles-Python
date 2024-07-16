9cb305ca37e7c474b8842ee114c2e6e924b4cde9
dink42 <liston.new@gmail.com>
2024-07-16 12:54:07 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Learning
----
-| Just a templet for me, if i forget how to play with turtles in python!
+# Python Turtles
+
+- First project in turles!
+
+> [!NOTE]
+> I have updated the code a bit, to be more clean, but basically the same as when first uploaded.
+> Could be a good way for new people to programming to play around with.
--- h.py
+++ h.py
@@ -1,16 +1,35 @@
 import turtle
+
+# Constants
+FORWARD_DISTANCE = 20
+TURN_ANGLE_INNER = 40
+TURN_ANGLE_OUTER = 45
+FULL_ROTATION = 30
+SECOND_FORWARD_DISTANCE = 10
+FINAL_TURN_ANGLE = 55
+REPEAT_TIMES = 20
+
+# Initialize the turtle
 pat = turtle.Turtle()
-for i in range(20):
-    for i in range(4):
-        pat.forward(100)
+
+# Main pattern drawing loop
+for _ in range(REPEAT_TIMES):
+    for _ in range(4):
+        pat.forward(FORWARD_DISTANCE)
         pat.color('green')
-        pat.right(38)
-        pat.forward(100)
+        pat.right(TURN_ANGLE_INNER)
+        
+        pat.forward(FORWARD_DISTANCE)
         pat.color('orange')
-        pat.right(40)
-    pat.right(36)
-    pat.color('red')
-    pat.forward(60)
-    pat.right(42)
-    pat.forward(100)
+        pat.right(TURN_ANGLE_OUTER)
     
+    pat.right(FULL_ROTATION)
+    pat.color('red')
+    
+    pat.forward(SECOND_FORWARD_DISTANCE)
+    pat.right(FINAL_TURN_ANGLE)
+    pat.forward(FORWARD_DISTANCE)
+
+# Hide the turtle and finish
+pat.hideturtle()
+turtle.done()
